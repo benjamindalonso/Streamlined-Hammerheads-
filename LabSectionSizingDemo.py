e762f916f1ba0773e0e123f5c1903b30887759c1
Ben Alonso <bdalonso@ucdavis.edu>
2026-02-02 20:41:12 -0800

    Landing constraint added

--- LabSectionSizingDemo.py
+++ LabSectionSizingDemo.py
@@ -19,11 +19,12 @@ WS = np.linspace(1,100,100)
 W0 = 67822  # Maximum takeoff weight from Assignment 2
 rhoRatio = 1 # assume standard day at sea level
 TOP25 = 1092/37.5 # Parametric value for takeoff performance on a cvn-78 carrier 
+Sland = 4000 # Target landing distance in feet without arresetment 
 
 
 # These equations need to be filled in after you find out the relationship between T/w and W/S for each constraint
 TW_takeoff = WS / (TOP25 * rhoRatio * C_L_max) # done by Mark
-TW_landing = 0.1 # Change after you find this relationship
+WS_landing = ((rhoRatio * C_L_max)/(80*0.6)) * (Sland-1000)
 TW_climb = 0.1 # Change after you find this relationship
 TW_cruise = 0.1 # Change after you find this relationship
 
@@ -33,6 +34,7 @@ plt.title('T/W - W/S')
 plt.xlabel("W/S $(lb/ft^2)$")
 plt.ylabel("T/W")
 plt.plot(WS, TW_takeoff, label='Takeoff field length', linestyle='-', linewidth=2)
+plt.axvline(x=WS_landing, color='green', linestyle='--', linewidth=2, label=f'Landing')
 # Add your other contraints here
 plt.ylim(0, 1.5)
 plt.legend(loc='best')
