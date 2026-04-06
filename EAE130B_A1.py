ac35aae0654cb69c4e731a22aeb1f937436f1758
Ben Alonso <bdalonso@ucdavis.edu>
2026-04-06 00:26:53 -0700

    Added tire size

--- EAE130B_A1.py
+++ EAE130B_A1.py
@@ -8,8 +8,10 @@ x_main = 35.164
 H = 7.1           
 
 n_main_wheels = 2
-n_nose_wheels = 1
+n_nose_wheels = 2
 n_braked_wheels = 2
+
+brake_time = 25 # How many seconds brakes are applied (this guess needs to be refined/sourced properly)
 
 V = 140             
 a_brake = 10       
@@ -38,6 +40,10 @@ KE_per_wheel = KE_total / n_braked_wheels
 KE_total_MJ = KE_total * 1.3558179483314 / 1e6
 KE_per_wheel_MJ = KE_per_wheel * 1.3558179483314 / 1e6
 
+# Convert to power units (energy per unit time)
+avg_power_total = KE_total / brake_time                    # ft·lbf / s
+avg_power_per_wheel = KE_per_wheel / brake_time            # ft·lbf / s  (per braked wheel)
+
 # results
 print("Wheelbase B =", round(B, 3), "ft")
 print("Nose gear to CG Mf =", round(Mf, 3), "ft")
@@ -59,3 +65,18 @@ print("Total KE =", round(KE_total, 1), "ft*lbf")
 print("KE per braked wheel =", round(KE_per_wheel, 1), "ft*lbf")
 print("Total KE =", round(KE_total_MJ, 3), "MJ")
 print("KE per braked wheel =", round(KE_per_wheel_MJ, 3), "MJ")
+print("Average power per wheel =", round(avg_power_per_wheel, 1), "ft*lbf/s")
+
+# Tire Sizing
+
+main_wheel_diameter = 1.59 * (main_load_per_wheel ** 0.302) # Fighter/trainer relation from raymer page 358
+main_wheel_width = 0.0980 * (main_load_per_wheel ** 0.467) # Fighter/trainer relation from raymer page 358
+nose_wheel_diameter = 0.7 * main_wheel_diameter # Nose wheel diameter is %60-%100 of main wheel diameter according to slide 47 of lecture 1
+nose_wheel_width = 0.7 * main_wheel_width # Lecture doesn't specify how to choose nose tire width, here we assume it's similar to the diameter
+
+print("Main wheel diameter =", round(main_wheel_diameter, 1), "in")
+print("Main wheel width =", round(main_wheel_width, 1), "in")
+print("Nose wheel diameter =", round(nose_wheel_diameter, 1), "in")
+print("Nose wheel width =", round(nose_wheel_width, 1), "in")
+
+# Tire sizes need to be compared to graph on page 50 of lecture 1 to determine if the wheels are big enough for the required brakes (see average power per wheel)
